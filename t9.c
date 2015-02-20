e99342738574226291bea3164781df8068b53247
Kevin Tsang <kevin@tsangfamily.us>
2015-02-20 01:48:52 -0800

    Removed README file and files are all compilable without errors.

--- t9.c
+++ t9.c
@@ -3,25 +3,26 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-void startInteractiveSession() {
+#include "list.h"
+#include "trie.h"
+
+void start_interactive_session() {
   char number[MAX_LINE_LENGTH];
   printf("%s", "Enter \"exit\" to quit.\n");
   while (1) {
     printf("%s", "Enter Key Sequence (or \"#\" for next word):\n");
     scanf("%s", number);
-    if (strncmp(number, "exit", 4) != 0) {
-      printf("%s", getWord(number));
-    } else {
-      freeTrie(trie->root);
-      break;
+    if (strncmp(number, "exit", 4) == 0) {
+      break; 
     }
+    printf("%s", get_word(number));
   }
 }
 
 int main(int argc, char** argv) {
-  wordsIndex = 0;
-  trie = (Trie*)malloc(sizeof(Trie));
-  readIntoTrie(argv[1]);
-  startInteractiveSession();
+  Trie* trie = (Trie*)malloc(sizeof(Trie));
+  trie_read(trie, argv[1]);
+  start_interactive_session();
+  trie_free(trie->root);
   return 0;
 }
--- trie.c
+++ trie.c
@@ -3,52 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_LENGTH 100
-
-typedef struct Node {
-  List* words;
-  struct Node* next[8];
-  int totalWords;
-} Node;
-
-typedef struct Trie {
-  Node* root;
-} Trie;
+#include "list.h"
+#include "trie.h"
 
 Trie* trie;
 Node* current;
 int wordsIndex;
 
 int determineIndex(char c) {
-  if (c == 'a' || c == 'b' || c == 'c') {
-    return 0;
-  } else if (c == 'd' || c == 'e' || c == 'f') {
-    return 1;
-  } else if (c == 'g' || c == 'h' || c == 'i') {
-    return 2;
-  } else if (c == 'j' || c == 'k' || c == 'l') {
-    return 3;
-  } else if (c == 'm' || c == 'n' || c == 'o') {
-    return 4;
-  } else if (c == 'p' || c == 'q' || c == 'r' || c == 's') {
-    return 5;
-  } else if (c == 't' || c == 'u' || c == 'v') {
-    return 6;
-  } else if (c == 'w' || c == 'x' || c == 'y' || c == 'z') {
-    return 7;
-  } else { // not found case
-    return -1;
+  switch(c) {
+    case 'a': case 'b': case 'c': return 0;
+    case 'd': case 'e': case 'f': return 1;
+    case 'g': case 'h': case 'i': return 2;
+    case 'j': case 'k': case 'l': return 3;
+    case 'm': case 'n': case 'o': return 4;
+    case 'p': case 'q': case 'r': case 's': return 5;
+    case 't': case 'u': case 'v': return 6;
+    case 'w': case 'x': case 'y': case 'z': return 7;
+    default: return -1;
   }
 }
 
-void readIntoTrie(char* filename) {
+void trie_read(Trie* trie_from_main, char* filename) {
   FILE* fp = fopen(filename, "r");
   char buffer[MAX_LINE_LENGTH];
+  trie = trie_from_main;
   trie->root = (Node*)malloc(sizeof(Node));
   current = trie->root;
   while (fgets(buffer, MAX_LINE_LENGTH, fp)) {
-    int i = 0;
-    for (; i < strnlen(buffer, MAX_LINE_LENGTH); i++) {
+    for (int i = 0; i < strnlen(buffer, MAX_LINE_LENGTH); i++) {
       if (buffer[i] == '\n') {
         break;
       }
@@ -65,32 +48,30 @@ void readIntoTrie(char* filename) {
     strncpy(word, buffer, MAX_LINE_LENGTH);
     List* currentWord = current->words;
     if (currentWord == NULL) {
-      current->words = createNewNode(word);
+      current->words = list_new(word);
     } else {
       while (currentWord->tail != NULL) {
         currentWord = currentWord->tail;
       }
-      currentWord->tail = createNewNode(word);
+      currentWord->tail = list_new(word);
     }
     current->totalWords++;
   }
 }
 
-char* getWord(char* number) {
+char* get_word(char* number) {
   if (number[0] == '#') {
     wordsIndex++;
     if (wordsIndex > current->totalWords) {
       return "There are no more T9nonyms.\n";
     }
-    int i = 0;
     List* currentWord = current->words;
-    return get(currentWord, wordsIndex);
+    return list_get(currentWord, wordsIndex);
   } else {
     current = trie->root;
     wordsIndex = 0;
-    int i = 0;
     int size = strnlen(number, MAX_LINE_LENGTH);
-    for (; i < size; i++) {
+    for (int i = 0; i < size; i++) {
       if (number[i] == '#') {
         wordsIndex++;
         if (wordsIndex > current->totalWords) {
@@ -98,24 +79,22 @@ char* getWord(char* number) {
         }
       }
       int index = determineIndex(number[i]);
-      if (current[index] == NULL) {
+      if (current->next[index] == NULL) {
         return "Not found in current dictionary.\n";
       } else {
-        current = current->[index];
+        current = current->next[index];
       }
     }
-    return get(current->words, wordsIndex);
+    return list_get(current->words, wordsIndex);
   }
 }
 
-void freeTrie(Node* root) {
-  int i = 0;
-  for (; i < 8; i++) {
+void trie_free(Node* root) {
+  for (int i = 0; i < 8; i++) {
     if (root->next[i] != NULL) {
-      freeTrie(root->next[i]);
-      free(root->next[i]);
+      trie_free(root->next[i]);
     }
   }
-  free_list(root->words);
+  list_free(root->words);
   free(root);
 }
